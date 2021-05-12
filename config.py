1e5919362569ccce6fb03aa30f62b66bb3bd3332
jasperazerbaijan <80984562+jasperazerbaijan@users.noreply.github.com>
2021-05-12 17:32:43 +0400

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
 # To use manual values, change these
-default_bot_token = "Your_Bot_Token_Here"
-default_sudo_chat_id =  -1234567777
-default_owner_id = 12345567
+default_bot_token = "1696209895:AAFBILSEK1piKsFYiuxVGwZSPSRqdwxoOVE"
+default_sudo_chat_id =  -1696209895
+default_owner_id = 940086446
 
 # Don't change these value
 bot_token = environ.get("BOT_TOKEN", default_bot_token)
